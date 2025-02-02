2010b7b7dd06f17fd409a915791939fdfb8717d1
OpsConverge <OpsConverge@gmail.com>
2025-02-01 23:37:33 -0600

    updated nextauth

--- ui/pages/api/auth/[...nextauth].ts
+++ ui/pages/api/auth/[...nextauth].ts
@@ -1,11 +1,11 @@
 import NextAuth from "next-auth";
-import Providers from "next-auth/providers";
+import GitHubProvider from 'next-auth/providers/github';
 
 export default NextAuth({
   providers: [
-    Providers.GitHub({
-      clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
     }),
   ],
   callbacks: {
