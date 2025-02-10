c5a117fcafd3dd3647876fab36fac8aca6de9282
OpsConverge <OpsConverge@gmail.com>
2025-02-10 16:25:44 -0600

    testing fix for issue

--- ui/lib/nextAuth.ts
+++ ui/lib/nextAuth.ts
@@ -272,13 +272,7 @@ export const getAuthOptions = (
 
   const authOptions: NextAuthOptions = {
     adapter,
-    providers: [
-      GitHubProvider({
-        clientId: process.env.GITHUB_CLIENT_ID as string,
-        clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
-        allowDangerousEmailAccountLinking: true,
-      }),
-    ],
+    providers,
     pages: {
       signIn: '/auth/login',
       verifyRequest: '/auth/verify-request',
@@ -364,7 +358,6 @@ export const getAuthOptions = (
       async session({ session, token, user }) {
         // When using JWT for sessions, the JWT payload (token) is provided.
         // When using database sessions, the User (user) object is provided.
-        session.accessToken = token.accessToken as string; // Pass access token to session
         if (session && (token || user)) {
           session.user.id = token?.sub || user?.id;
         }
@@ -396,14 +389,8 @@ export const getAuthOptions = (
           return { ...token, name: session.name };
         }
 
-        if (account) {
-          token.accessToken = account.access_token; // Store GitHub access token
-        }
-
         return token;
       },
-
-      
     },
     jwt: {
       encode: async (params) => {
--- ui/pages/api/auth/[...nextauth].ts
+++ ui/pages/api/auth/[...nextauth].ts
@@ -1,28 +1,3 @@
-// import NextAuth from "next-auth";
-// import GitHubProvider from 'next-auth/providers/github';
-
-// export default NextAuth({
-//   providers: [
-//     GitHubProvider({
-//       clientId: process.env.GITHUB_CLIENT_ID as string,
-//       clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
-//     }),
-//   ],
-//   callbacks: {
-//     async session(session, token) {
-//       session.accessToken = token.accessToken; // Add GitHub access token to the session
-//       return session;
-//     },
-//     async jwt(token, user, account) {
-//       if (account) {
-//         token.accessToken = account.access_token; // Store GitHub access token in JWT
-//       }
-//       return token;
-//     },
-//   },
-//   secret: process.env.NEXTAUTH_SECRET,
-// });
-
 
 import NextAuth from 'next-auth';
 import type { NextApiRequest, NextApiResponse } from 'next';
